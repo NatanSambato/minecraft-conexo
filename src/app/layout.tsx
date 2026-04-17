6e7e1c98ce6aefc822ded2979e1e8fe8a6dadd8e
NatanSambato <natan_sambato@hotmail.com>
2026-04-17 15:18:40 -0300

    Added a src folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import localFont from 'next/font/local'
 import "./globals.css";
 
 const minecraftFont = localFont({
-  src: '../public/fonts/minecraft.woff2',
+  src: '../../public/fonts/minecraft.woff2',
   variable: '--font-minecraft',
 })
 
