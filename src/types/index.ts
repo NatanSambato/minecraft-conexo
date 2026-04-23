06c16221f4a73c2b9672451f5c6e368d9405ec81
NatanSambato <natan_sambato@hotmail.com>
2026-04-23 13:07:54 -0300

    Added index.ts with game types

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export interface Tile {
+  id:      string
+  label:   string
+  groupId: number
+}
+
+export interface Group {
+  id:          number
+  correlation: string
+  color:       string
+  items:       string[]
+}
+
+export interface Puzzle {
+  date:   string
+  groups: Group[]
+}
+
+export type GameStatus = 'playing' | 'won'
