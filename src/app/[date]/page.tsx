8287508d91eb30b2c9226d19eb804a7c169debca
NatanSambato <natan_sambato@hotmail.com>
2026-04-24 17:07:53 -0300

    Created a [date] route to render the game

---
+++ src/app/[date]/page.tsx
@@ -0,0 +1,24 @@
+import Board from './components/Board'
+
+import { notFound } from 'next/navigation'
+import fs from 'fs'
+import path from 'path'
+import { getTodaysDate } from '@/lib/gameUtils'
+
+export default async function GamePage({ params }: { params: Promise<{ date: string }> }) {
+    const { date } = await params
+    const filePath = path.join(process.cwd(), 'src', 'app', 'data', 'puzzles', `${date}.json`)
+
+    if (!fs.existsSync(filePath)) {
+        console.log("File does not exist")
+        notFound() 
+    }
+    if ((date) > getTodaysDate()) {
+        console.log("That game is in the future")
+        notFound()
+    }
+    const raw = fs.readFileSync(filePath, 'utf-8')
+    const puzzle = JSON.parse(raw)
+    
+    return <Board puzzle={puzzle}/>
+}
