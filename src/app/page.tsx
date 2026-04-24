8087e384a3ccd4977e04a7bb89ff566a09143fd9
NatanSambato <natan_sambato@hotmail.com>
2026-04-24 17:09:44 -0300

    Updated homepage layout with Daily Game and Archives buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { getTodaysDate } from '@/lib/gameUtils'
+import Link from 'next/link';
 
 export default function page() {
+  const today = getTodaysDate();
+
   return (
-    <div>
-      Initial Setup
-    </div>
+    <main className='flex flex-col items-center justify-center min-h-screen gap-8'>
+      <h1 className='text-xl'>MINECRAFT CONEXO</h1>
+
+      <div className='flex flex-col gap-4'>
+        <Link href={`/${today}`} className="px-6 py-3 bg-green-600 text-white rounded">
+          Daily Game
+        </Link>
+
+        <Link href={"/archive"} className='px-6 py-3 bg-gray-600 text-white text-center rounded'>
+          Archive
+        </Link>
+      </div>
+    </main>
   )
 }
