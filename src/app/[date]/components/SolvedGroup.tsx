493e48f22e8389ee445f20063f1a876d74461615
NatanSambato <natan_sambato@hotmail.com>
2026-05-09 09:20:34 -0300

    fix: match solved group height to tile height

--- src/app/[date]/components/SolvedGroup.tsx
+++ src/app/[date]/components/SolvedGroup.tsx
@@ -5,7 +5,7 @@ export default function SolvedGroup({ group }: { group: Group }) {
   const bgColor = getGroupColor(group.color)
 
   return (
-    <div className={`flex flex-col items-center justify-center p-1.5 rounded-lg ${bgColor}`}>
+    <div className={`flex flex-col items-center justify-center p-1.5 h-20 rounded-lg ${bgColor}`}>
       <span className='font-bold text-lg'>{group.correlation}</span>
       <span className='uppercase'>{group.items.join(', ')}</span>
     </div>
