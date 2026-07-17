e6dc4eae19beba6aaf9e5d15b7993a25e6ca90bc
NatanSambato <32022776+NatanSambato@users.noreply.github.com>
2026-07-17 19:57:15 -0300

    feat(board): add enchanted book overlay with label for enchantment entry tiles

--- src/components/TileCard.tsx
+++ src/components/TileCard.tsx
@@ -1,4 +1,5 @@
 import { getEffectIcon } from "@/lib/effectIcons";
+import { isItemAnEnchantment } from "@/lib/enchantments";
 import Image from "next/image";
 
 interface Props {
@@ -23,6 +24,7 @@ export default function TileCard({
   const isWaxed = label.startsWith("Waxed ");
   const effectIcon = getEffectIcon(label);
   const isArrow = label.startsWith("Arrow of");
+  const isEnchantment = isItemAnEnchantment(label);
 
   const getFontSize = (label: string) => {
     if (label.length > 24) return "text-xs";
@@ -36,7 +38,7 @@ export default function TileCard({
       onClick={onClick}
       disabled={disabled}
       className={`
-                relative h-20 flex items-center justify-center rounded-sm p-4 text-center leading-5 font-bold uppercase
+                relative h-20 flex items-center justify-center rounded-sm text-center leading-5 font-bold uppercase
                 ${isSelected ? "bg-amber-600" : "bg-stone-600"}
                 ${disabled ? "" : "cursor-pointer"}
             `}
@@ -56,11 +58,32 @@ export default function TileCard({
       )}
 
       {/* Text Placeholder */}
-      {label && !image && (
+      {label && !image && !isEnchantment && (
         <>
           <span className={`${getFontSize(label)} leading-tight`}>{label}</span>
           <span className={`absolute top-1 right-1 text-amber-400`}>⚠ </span>
         </>
+      )}
+
+      {/* Enchantments Overlay */}
+      {isEnchantment && (
+        <div className="absolute inset-0 flex flex-col items-center">
+          <div className="relative w-full flex-1 min-h-0">
+            <Image
+              src="/images/tile-overlays/enchantments-overlay.gif"
+              alt="enchantment"
+              fill
+              sizes="(max-width: 576px) 25vw, 144px"
+              unoptimized
+              className="object-contain pointer-events-none shrink-0"
+            />
+          </div>
+          <span
+            className={`pointer-events-none leading-3 py-0.5 ${label.length >= 18 ? "text-xs" : "text-sm"} `}
+          >
+            {label}
+          </span>
+        </div>
       )}
 
       {/* Wax Overlay */}
@@ -74,7 +97,7 @@ export default function TileCard({
         />
       )}
 
-      {/* Effect */}
+      {/* Potion && Arrows Effect Overlay */}
       {effectIcon && (
         <div
           className={`absolute w-5 h-5 ${isArrow ? "bottom-1.5 right-3.5" : "top-1.5 right-3.5"}`}
